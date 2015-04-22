929727e5aa4eeeddf790e248ce0214fec9588c8f
Frederick Topico <ftopico@exist.com>
2015-04-22 09:04:14 +0800

    initial structure

---
+++ solcc-library-core/src/main/java/com/solcc/library/core/App.java
@@ -0,0 +1,13 @@
+package com.solcc.library.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
