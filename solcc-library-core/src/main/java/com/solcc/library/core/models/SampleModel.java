2451ab49f14aab8683d5609ca8c42cdd2f0be314
Frederick Topico <ftopico@exist.com>
2015-05-11 08:50:38 +0800

    sample model

---
+++ solcc-library-core/src/main/java/com/solcc/library/core/models/SampleModel.java
@@ -0,0 +1,75 @@
+package com.solcc.library.core.models;
+
+public class SampleModel {
+
+	private String member1;
+	
+	private String member2;
+	
+	private String camelCasingMustBeUsed;
+	
+	private String learnToUseMembers;
+	
+	private String thatCanEasilyBeRead;
+	
+	private String useFullWordsInstead;
+	
+	private String ofAmbigousWords;
+
+	public String getMember1() {
+		return member1;
+	}
+
+	public void setMember1(String member1) {
+		this.member1 = member1;
+	}
+
+	public String getMember2() {
+		return member2;
+	}
+
+	public void setMember2(String member2) {
+		this.member2 = member2;
+	}
+
+	public String getCamelCasingMustBeUsed() {
+		return camelCasingMustBeUsed;
+	}
+
+	public void setCamelCasingMustBeUsed(String camelCasingMustBeUsed) {
+		this.camelCasingMustBeUsed = camelCasingMustBeUsed;
+	}
+
+	public String getLearnToUseMembers() {
+		return learnToUseMembers;
+	}
+
+	public void setLearnToUseMembers(String learnToUseMembers) {
+		this.learnToUseMembers = learnToUseMembers;
+	}
+
+	public String getThatCanEasilyBeRead() {
+		return thatCanEasilyBeRead;
+	}
+
+	public void setThatCanEasilyBeRead(String thatCanEasilyBeRead) {
+		this.thatCanEasilyBeRead = thatCanEasilyBeRead;
+	}
+
+	public String getUseFullWordsInstead() {
+		return useFullWordsInstead;
+	}
+
+	public void setUseFullWordsInstead(String useFullWordsInstead) {
+		this.useFullWordsInstead = useFullWordsInstead;
+	}
+
+	public String getOfAmbigousWords() {
+		return ofAmbigousWords;
+	}
+
+	public void setOfAmbigousWords(String ofAmbigousWords) {
+		this.ofAmbigousWords = ofAmbigousWords;
+	}
+	
+}
